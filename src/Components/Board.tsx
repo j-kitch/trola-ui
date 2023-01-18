e15f3b3a71bf8c1f347b07b88282942ff5713f0c
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-01-18 16:34:41 +0000

    Added some dummy list/card data

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,18 +1,41 @@
 import React from "react";
 import {useParams} from "react-router";
-import {Typography} from "@mui/material";
+import {Box, Card, CardContent, Grid, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
 import {withAuthenticationRequired} from "@auth0/auth0-react";
+import BoardState from "../State/BoardState";
 
 function Board() {
+
     const {id} = useParams();
+
+    const board = BoardState.find(b => b.id === id)!;
+
     return (
-        <div>
-            <Typography>This is the board {id}</Typography>
-            <Link to="/boards">
-                <Typography>Go back to boards</Typography>
-            </Link>
-        </div>
+        <Box>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography>{board.name}</Typography>
+                    <Link to="/boards">
+                        <Typography>Back to boards</Typography>
+                    </Link>
+                </Grid>
+                {board.lists.map(list => (
+                    <Grid item xs={3}>
+                        <Box sx={{ background: "#aaaaaa", display: "flex", flexDirection: "column" }}>
+                            <Typography>{list.name}</Typography>
+                            {list.cards.map(card => (
+                                <Card>
+                                    <CardContent>
+                                        {card.title}
+                                    </CardContent>
+                                </Card>
+                            ))}
+                        </Box>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
     );
 }
 
