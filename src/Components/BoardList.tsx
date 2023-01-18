792b0798fcc2a61858ec908b67027ea553f08860
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-01-18 16:25:03 +0000

    Added some dummy data to BoardList view

--- src/Components/BoardList.tsx
+++ src/Components/BoardList.tsx
@@ -1,16 +1,47 @@
 import React from "react";
-import {Typography} from "@mui/material";
+import {Box, Card, CardActionArea, CardContent, Grid, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
 import {withAuthenticationRequired} from "@auth0/auth0-react";
+import Boards from "../State/BoardState";
+import AddIcon from "@mui/icons-material/Add";
 
 function BoardList() {
     return (
-        <div>
-            <Typography>Boards List</Typography>
-            <Link to="/boards/10">
-                <Typography>Go to board 10</Typography>
-            </Link>
-        </div>
+        <Box>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography>These are your current boards.</Typography>
+                </Grid>
+                {Boards.map(board => (
+                    <Grid item xs={3}>
+                        <Card>
+                            <Link to={`/boards/${board.id}`}>
+                                <CardActionArea>
+                                    <CardContent>
+                                        <Typography>{board.name}</Typography>
+                                        <Typography>
+                                            {board.activeCards > 0 && `There are ${board.activeCards} tickets that require your attention.`}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Link>
+                        </Card>
+                    </Grid>
+                ))}
+                <Grid item xs={3}>
+                    <Card>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography>Create a new board</Typography>
+                                    <Typography>
+                                        <AddIcon fontSize="large"/>
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
 
---
+++ src/State/BoardState.ts
@@ -0,0 +1,14 @@
+const Boards = [
+    {
+        id: '1',
+        name: 'Foo Board',
+        activeCards: 10,
+    },
+    {
+        id: '2',
+        name: "Bar Board",
+        activeCards: 0,
+    }
+];
+
+export default Boards;
