541988ced5b71228da0589a32a3ece500a4e4ace
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-01-28 16:19:18 +0000

    Connected to auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
 import React, {FC, useEffect, useState} from 'react';
 import LoginButton from "./LoginButton";
+import {Auth0Provider} from "@auth0/auth0-react";
+
+const delay = (millis: number) => new Promise<void>(resolve => setTimeout(resolve, millis));
 
 const App: FC = () => {
 
-    const [isVisible, setIsVisible] = useState(false);
+    const delayTimeout = 1000;
+
+    const [isWelcomeVisible, setIsWelcomeVisible] = useState(false);
+    const [isLoginVisible, setIsLoginVisible] = useState(false);
 
     useEffect(() => {
-        setInterval(() => setIsVisible(true), 1000);
+        delay(delayTimeout)
+            .then(() => setIsWelcomeVisible(true))
+            .then(() => delay(delayTimeout))
+            .then(() => setIsLoginVisible(true));
     }, []);
 
-    const opacityClass = isVisible ? "opacity-100" : "opacity-0";
+    const opacityClass = isWelcomeVisible ? "opacity-100" : "opacity-0";
 
     return (
-        <div className="flex h-screen w-screen bg-gradient-to-br from-cyan-400 to-blue-600 justify-center items-center">
-            <div className="flex flex-col gap-4 justify-center items-center">
-                <h1 className="text-white text-7xl">
-                <span className={`${opacityClass} transition-opacity ease-in-out duration-1000`}>
-                    Welcome to Trolá!
-                </span>
-                </h1>
-                <LoginButton/>
+        <Auth0Provider
+            domain="dev-cstaap57nsxcjdlq.us.auth0.com"
+            clientId="02HOjjGMw6pMZpmxg2b3ZZU3uGcJ11HT"
+            authorizationParams={{
+                redirect_uri: window.location.origin
+            }}
+        >
+            <div
+                className="flex h-screen w-screen bg-gradient-to-br from-cyan-400 to-blue-600 justify-center items-center">
+                <div className="flex flex-col gap-4 justify-center items-center">
+                    <h1 className="text-white text-7xl">
+                        <span className={`${opacityClass} transition-opacity ease-in-out duration-1000`}>
+                            Welcome to Trolá!
+                        </span>
+                    </h1>
+                    <LoginButton isVisible={isLoginVisible}/>
+                </div>
             </div>
-        </div>
+        </Auth0Provider>
     );
 };
 
--- src/LoginButton.tsx
+++ src/LoginButton.tsx
@@ -1,10 +1,27 @@
 import React, {FC} from "react";
+import {useAuth0} from "@auth0/auth0-react";
 
-const LoginButton: FC = () => {
+interface Props {
+    isVisible: boolean
+}
+
+const LoginButton: FC<Props> = ({isVisible}) => {
+
+    const auth0 = useAuth0();
+
+    const onLogin = () => {
+        auth0.loginWithPopup();
+    };
+
+    const visibleClass = isVisible ? "opacity-100" : "opacity-0";
+
     return (
-        <button className="text-gray-100 text-lg border-gray-200 w-3/12
-                            hover:bg-white hover:text-blue-600 transition-colors duration-500
-                            bg-opacity-50 rounded-2xl">
+        <button onClick={onLogin}
+                className={`text-gray-100 text-lg border-gray-200 w-5/12
+                            border-2 border-gray-100 border-opacity-30 hover:border-white
+                            hover:bg-white hover:text-blue-600 transition-all
+                            bg-opacity-50 rounded-2xl
+                            w-auto transition-opacity ease-in-out duration-1000 ${visibleClass}`}>
             Login or Sign Up
         </button>
     );
