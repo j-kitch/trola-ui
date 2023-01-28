1b556ac7fa44857104fd6545684137d2cda0e2cc
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-01-28 15:16:19 +0000

    Remove all dependencies and begin a rewrite with react, typescript and tailwindcss

--- src/configuration/auth0.ts
+++
@@ -1,5 +0,0 @@
-export const auth0Configuration = {
-    domain: "dev-cstaap57nsxcjdlq.us.auth0.com",
-    clientId: "02HOjjGMw6pMZpmxg2b3ZZU3uGcJ11HT",
-    redirectUri: window.location.origin,
-};
