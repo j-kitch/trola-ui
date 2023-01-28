bfaceb905a760efc715a36be1cbc0c73deeaeaf7
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-01-28 15:44:55 +0000

    Added a welcome screen with a transition intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 
-const App: FC = () => (
-    <h1 className="text-3xl font-bold underline">
-        Hello World!
-    </h1>
-);
+const App: FC = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setInterval(() => setIsVisible(true), 1000);
+    }, []);
+
+    const opacityClass = isVisible ? "opacity-100" : "opacity-0";
+
+    return (
+        <div className="flex h-screen w-screen bg-gradient-to-br from-cyan-400 to-blue-600 justify-center items-center">
+            <h1 className="text-white text-7xl">
+                <span className={`${opacityClass} transition-opacity ease-in-out duration-1000`}>
+                    Welcome to Trolá!
+                </span>
+            </h1>
+        </div>
+    );
+};
 
 export default App;
