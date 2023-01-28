cc89579e5b39cc80d31f6c6163177b3b9b9ad693
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-01-28 15:08:03 +0000

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,26 @@
-import React from "react";
-import {CssBaseline, CssVarsProvider} from "@mui/joy";
-import Root from "./layout/Root";
-import Header from "./layout/Header";
-import Main from "./layout/Main";
-import theme from "./theme";
-import AppBar from "./component/AppBar";
-import WelcomePage from "./page/WelcomePage";
-import {Auth0Provider} from "@auth0/auth0-react";
-import {auth0Configuration} from "./configuration/auth0";
-import UserPage from "./page/UserPage";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import NotFoundPage from "./page/NotFoundPage";
-import ErrorPage from "./page/ErrorPage";
-import IndexPage from "./page/IndexPage";
-import NavigateToHome from "./component/NavigateToHome";
-import TeamsPage from "./page/TeamsPage";
-import BoardsPage from "./page/BoardsPage";
-import TeamPage from "./page/TeamPage";
-import NewTeamPage from "./page/NewTeamPage";
-import {TeamProvider} from "./context/TeamContext";
-import {UserProvider} from "./context/UserContext";
-import {BoardProvider} from "./context/BoardContext";
-import BoardPage from "./page/BoardPage";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
-    return (
-        <CssVarsProvider theme={theme}>
-            <Auth0Provider {...auth0Configuration}>
-                <TeamProvider>
-                    <UserProvider>
-                        <BoardProvider>
-                            <BrowserRouter>
-                                <CssBaseline/>
-                                <Root>
-                                    <Header>
-                                        <AppBar/>
-                                    </Header>
-                                    <NavigateToHome/>
-                                    <Main>
-                                        <Routes>
-                                            <Route index element={<IndexPage/>} errorElement={<ErrorPage/>}/>
-                                            <Route path="/welcome" element={<WelcomePage/>}
-                                                   errorElement={<ErrorPage/>}/>
-                                            <Route path="/user" element={<UserPage/>} errorElement={<ErrorPage/>}/>
-                                            <Route path="/teams" element={<TeamsPage/>} errorElement={<ErrorPage/>}/>
-                                            <Route path="/teams/:id" element={<TeamPage/>} errorElement={<ErrorPage/>}/>
-                                            <Route path="/teams/new" element={<NewTeamPage/>}
-                                                   errorElement={<ErrorPage/>}/>
-                                            <Route path="/teams/:teamId/boards/:boardId" element={<BoardPage/>} errorElement={<ErrorPage/>}/>
-                                            <Route path="/boards" element={<BoardsPage/>} errorElement={<ErrorPage/>}/>
-                                            <Route path="*" element={<NotFoundPage/>} errorElement={<ErrorPage/>}/>
-                                        </Routes>
-                                    </Main>
-                                </Root>
-                            </BrowserRouter>
-                        </BoardProvider>
-                    </UserProvider>
-                </TeamProvider>
-            </Auth0Provider>
-        </CssVarsProvider>
-    );
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
