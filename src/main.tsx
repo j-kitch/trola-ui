56952d1ff3d80f08033ed1a51f444ba8003509d0
Joshua Kitchen <joshua.kitchen@northerncode.uk>
2023-02-02 09:23:49 +0000

    Replaced modern-normalize with tailwind preflight

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App, AppEnv } from './App';
 import './index.css';
-import 'modern-normalize/modern-normalize.css';
+import 'tailwindcss/src/css/preflight.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
